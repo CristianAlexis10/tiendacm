163fa512c8beafbc700a2ac41cd119db141a40c0
CristianAlexis10 <cristian1020011@gmail.com>
2017-11-04 23:42:25 -0700

    22

---
+++ controller/views.controller.php
@@ -0,0 +1,18 @@
+<?php
+class ViewsController{
+	function mainPage(){
+		 require_once("views/include/header.php");
+     		 require_once("views/modules/landing.php");
+    		  require_once("views/include/footer.php");
+	}
+
+	function dashboard(){
+		 require_once ("views/include/header.php");
+     		 require_once ("views/modules/dashboard/index.php");
+     		 require_once ("views/include/footer.php");
+	}
+	function landin(){
+     		 require_once ("views/modules/landing.php");
+	}
+}
+?>
