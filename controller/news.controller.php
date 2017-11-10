1e28dc77339c837a98895b50b3438241ec231e12
CristianAlexis10 <cristian1020011@gmail.com>
2017-11-09 18:33:25 -0800

    5

---
+++ controller/news.controller.php
@@ -0,0 +1,21 @@
+<?php
+
+// require_once("model/tienda.model.php");
+Class NewsController{
+    private $TiendaM;
+
+    public function __CONSTRUCT(){
+      // $this->TiendaM = new TiendaModel();
+    }
+
+    public function mainPage(){
+             require_once "views/include/header.php";
+             require_once "views/modules/news/index.php";
+            require_once "views/include/footer.php";
+    }
+
+    
+
+}
+
+ ?>
