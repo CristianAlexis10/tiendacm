f6a2e0721e4651598440840b9a539ee9401817d6
darckblade <jdgiraldo08@misena.edu.co>
2017-11-15 11:20:04 -0500

    19

--- views/include/usuario/footer.php
+++ views/include/usuario/footer.php
@@ -1,9 +1,5 @@
     </section>
-<<<<<<< HEAD
-    <script src="views/assets/js/main.js"></script>
-=======
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="views/assets/js/main.js"></script>
->>>>>>> 2a2754c91e81e0dd1140dd4c0e6780f69655caf1
+    <script src="views/assets/js/main.js"></script>
   </body>
 </html>
