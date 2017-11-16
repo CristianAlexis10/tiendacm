74e712891433223701aac8ab752ba6cbf12b04d5
darckblade <jdgiraldo08@misena.edu.co>
2017-11-16 17:12:45 -0500

    20

--- views/include/login-registro.php
+++ views/include/login-registro.php
@@ -1,59 +1,59 @@
 <div class="modal" id="modal-login-registro">
-  <div class="login" id='login'>
-    <form class="formulario" id="formulario-login" action="index.html" method="post">
-      <div class="titulo">
-        <span id="cerrar_modal" class="cerrar"><i class="fa fa-times" aria-hidden="true"></i></span>
-        <h2>INICIAR SESIÓN</h2>
-      </div>
-      <div class="wrap-content-form">
-          <p>Ingrese el correo electronico y la contraseña para poder acceder al sistema</p>
-          <div class="caja_usuario">
-            <input type="text" name="data-login" id="login-email" class="input" required>
-            <label for="login-email" class="label">Correo</label>
+      <div class="login" id='login'>
+        <form class="formulario" id="formulario-login" action="index.html" method="post">
+          <div class="titulo">
+            <span id="cerrar_modal" class="cerrar"><i class="fa fa-times" aria-hidden="true"></i></span>
+            <h2>INICIAR SESIÓN</h2>
           </div>
-          <div class="caja_contra">
-            <input type="password" name="data-login" id="login-pass" class="input" required>
-            <label for="login-pass" class="label" >Contraseña</label>
+          <div class="wrap-content-form">
+              <p>Ingrese el correo electronico y la contraseña para poder acceder al sistema</p>
+              <div class="caja_usuario">
+                <input type="text" name="data-login" id="login-email" class="input" required>
+                <label for="login-email" class="label">Correo</label>
+              </div>
+              <div class="caja_contra">
+                <input type="password" name="data-login" id="login-pass" class="input" required>
+                <label for="login-pass" class="label" >Contraseña</label>
+              </div>
+              <div class="btn_inicio">
+                <button type="submit" class="ini_sesion">Iniciar sesion</button>
+                <button type="button" class="ir_registro" id="btn_registro">registro</button>
+              </div>
           </div>
-          <div class="btn_inicio">
-            <button type="submit" class="ini_sesion">Iniciar sesion</button>
-            <button type="button" class="ir_registro" id="btn_registro">registro</button>
+        </form>
+      </div>
+      <div class="registro" id="registro">
+        <form class="formulario" action="index.html" method="post">
+          <div class="titulo">
+            <h2>REGISTRATE</h2>
           </div>
+          <div class="wrap-content-form">
+            <p>Ingrese lo datos correspondientes para poder Registrarse en el sistema</p>
+            <div class="caja_nombre">
+              <input type="text" class="input" required>
+              <label for=""class="label">Nombre</label>
+            </div>
+            <div class="caja_apellido">
+              <input type="text" class="input" required>
+              <label for="" class="label">Apellidos</label>
+            </div>
+            <div class="caja_correo">
+              <input type="text" class="input" required>
+              <label for="" class="label">Correo</label>
+            </div>
+            <div class="caja_pass">
+              <input type="text" class="input" required>
+              <label for="" class="label">Contraseña</label>
+            </div>
+            <div class="caja_conpass">
+              <input type="text" class="input" required>
+              <label for="" class="label">Confirmar contraseña</label>
+            </div>
+          </div>
+            <div class="btn_registro">
+              <button type="submit" class="btn_registrarse">Registrarse</button>
+              <button type="button" id="btn_login" class="ir_registro">Iniciar sesión</button>
+            </div>
+        </form>
       </div>
-    </form>
-  </div>
-  <div class="registro" id="registro">
-    <form class="formulario" action="index.html" method="post">
-      <div class="titulo">
-        <h2>REGISTRATE</h2>
-      </div>
-      <div class="wrap-content-form">
-        <p>Ingrese lo datos correspondientes para poder Registrarse en el sistema</p>
-        <div class="caja_nombre">
-          <input type="text" class="input" required>
-          <label for=""class="label">Nombre</label>
-        </div>
-        <div class="caja_apellido">
-          <input type="text" class="input" required>
-          <label for="" class="label">Apellidos</label>
-        </div>
-        <div class="caja_correo">
-          <input type="text" class="input" required>
-          <label for="" class="label">Correo</label>
-        </div>
-        <div class="caja_pass">
-          <input type="text" class="input" required>
-          <label for="" class="label">Contraseña</label>
-        </div>
-        <div class="caja_conpass">
-          <input type="text" class="input" required>
-          <label for="" class="label">Confirmar contraseña</label>
-        </div>
-      </div>
-        <div class="btn_registro">
-          <button type="submit" class="btn_registrarse">Registrarse</button>
-          <button type="button" id="btn_login" class="ir_registro">Iniciar sesión</button>
-        </div>
-    </form>
-  </div>
-</div>
+    </div>
--- views/modules/tienda/tienda.php
+++ views/modules/tienda/tienda.php
@@ -1,33 +1,33 @@
     <section>
-      <div class="seccion1">
-        <div class="slider">
-          <div class="wrap--text">
-            <h2>TIENDA</h2>
-            <h3>Cambia tu estilo con Catalina Molano</h3>
-          </div>
-        </div>
-      </div>
-      <div class="seccion2">
-        <?php foreach ($consulta as $row) { ?>
-        <div class="item">
-          <div class="caja--img">
-            <img src="views/assets/img/<?php echo $row["pro_img"]; ?>">
-          </div>
-          <div class="descripcion">
-            <h2><?php echo $row["pro_nombre"]; ?></h2>
-            <article><?php echo $row["pro_des"]; ?></article>
-            <div class="descripcion--footer">
-              <div class="btn--carrito">
-                <button type="button">Añadir al carrito</button>
-              </div>
-              <div class="btn--mas_menos">
-                <button type="button" class="menos">-</button>
-                <label>0</label>
-                <button type="button" class="mas">+</button>
-              </div>
+        <div class="seccion1">
+          <div class="slider">
+            <div class="wrap--text">
+              <h2>TIENDA</h2>
+              <h3>Cambia tu estilo con Catalina Molano</h3>
             </div>
           </div>
         </div>
-      <?php  } ?>
-      </div>
-    </section>
+        <div class="seccion2">
+          <?php foreach ($consulta as $row) { ?>
+<div class="item">
+            <div class="caja--img">
+              <img src="views/assets/img/<?php echo $row["pro_img"]; ?>">
+            </div>
+            <div class="descripcion">
+              <h2><?php echo $row["pro_nombre"]; ?></h2>
+              <article><?php echo $row["pro_des"]; ?></article>
+              <div class="descripcion--footer">
+                <div class="btn--carrito">
+                  <button type="button">Añadir al carrito</button>
+                </div>
+                <div class="btn--mas_menos">
+                  <button type="button" class="menos">-</button>
+                  <label>0</label>
+                  <button type="button" class="mas">+</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php  } ?>
+</div>
+      </section>
