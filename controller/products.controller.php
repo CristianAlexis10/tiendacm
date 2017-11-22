28687e8f90ab7a4a0a12769f4e114259b59eaaed
Cristisn Lopera <cristian1020011@gmail.com>
2017-11-22 13:09:44 -0500

    43

---
+++ controller/products.controller.php
@@ -0,0 +1,11 @@
+<?php
+class ProductsController{
+	function mainPage(){
+		 require_once("views/include/dashboard/header.php");
+     	require_once("views/modules/tienda/new.productos.php");
+     	require_once("views/include/dashboard/footer.php");
+	}
+
+
+}
+?>
