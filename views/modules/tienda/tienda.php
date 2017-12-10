812188308b5b4dfe8430bc5585e1cee013daf216
CristianAlexis10 <cristian1020011@gmail.com>
2017-12-10 15:28:05 -0500

    hghg

--- views/modules/tienda/tienda.php
+++ views/modules/tienda/tienda.php
@@ -5,24 +5,95 @@
   </div>
 </div>
 <div class="seccion2">
-  <div class="wrap-items">
-    <?php $result = $this->master->selectAll('producto');
-    foreach ($result as $row) {
-      ?>
-    <div class="item">
-      <div class="wrap-img">
-        <img src="views/assets/img/products/<?php echo $row['pro_imagen'] ?>" id="<?php echo $row['pro_codigo']?>" alt="" class="wea">
-      </div>
-      <div class="nombre-produc">
-        <h2><?php echo $row['pro_nombre']; ?></h2>
-      </div>
-      <div class="precio-produc">
-        <h2>$ <?php echo $row['pro_precio']; ?></h2>
-      </div>
-      <button type="button" name="button" class="abrirEsaWea">añadir al carrito</button>
-    </div>
-    <?php } ?>
-  </div>
+  <form method="post">
+      <select name="categoria">
+        <option value="">seleccionar categoria</option>
+        <?php
+          foreach ($this->master->selectAll('categoria') as $row) {
+              if (isset($_SESSION['CATEGORIA'])) {
+                  if ($row['cat_codigo']==$_SESSION['CATEGORIA']) {?>
+                        <option value="<?php echo $row['cat_codigo']?>" selected ><?php echo $row['cat_nombre']?></option>
+                  <?php }else{?>
+                        <option value="<?php echo $row['cat_codigo']?>"  ><?php echo $row['cat_nombre']?></option>
+                  <?php }
+              }else{?>
+                        <option value="<?php echo $row['cat_codigo']?>" ><?php echo $row['cat_nombre']?></option>
+
+              <?php
+              } } ?>
+      </select>
+      <input type="submit" value="Enviar" name="submit">
+  </form>
+
+  <?php
+       //total de elementos por cada pagina
+      $elementosPagina = 12;
+      //saber si existe la pagina
+      if (isset($_GET["pagina"])) {
+        $pagina = $_GET["pagina"];
+         $inicio = ($pagina - 1) * $elementosPagina;
+      }else{
+         $inicio = 0;
+         $pagina = 1;
+      }
+      if (isset($_POST['submit'])) {
+              $_SESSION['CATEGORIA']= $_POST['categoria'];
+              $consulta = $this->master->selectAllLimitWhere('producto',$inicio,$elementosPagina,$_SESSION['CATEGORIA']);
+              $num_total_registros = $this->master->selectCount('producto','cat_codigo',$_SESSION['CATEGORIA']);
+      }else{
+              $consulta = $this->master->selectAllLimit('producto',$inicio,$elementosPagina);
+              $num_total_registros = $this->master->selectAllCount('producto');
+      }
+
+      //total paginas
+      $total_paginas = ceil($num_total_registros[0]/$elementosPagina);
+      if ($pagina>$total_paginas){
+          echo "Pagina no disponible";
+      }?>
+      <div class="wrap-items">
+        <?php
+      foreach ($consulta as $row ) {?>
+        <div class="item">
+          <div class="wrap-img">
+            <img src="views/assets/img/products/<?php echo $row['pro_imagen'] ?>" id="<?php echo $row['pro_codigo']?>" alt="" class="wea">
+          </div>
+          <div class="nombre-produc">
+            <h2><?php echo $row['pro_nombre']; ?></h2>
+          </div>
+          <div class="precio-produc">
+            <h2>$ <?php echo $row['pro_precio']; ?></h2>
+          </div>
+          <button type="button" name="button" class="abrirEsaWea">añadir al carrito</button>
+        </div>
+
+      <?php
+      }?>
+        </div>
+        <?php
+
+        if ($total_paginas > 1) {
+
+           if ($pagina != 1){
+              echo '<a href="catalogo-'.($pagina-1).'"><i class="fa fa-arrow-left"></i></a>';
+           }
+              for ($i=1;$i<=$total_paginas;$i++) {
+                 if ($pagina == $i){
+                    //si muestro el índice de la página actual, no coloco enlace
+                    echo $pagina;
+                 }else{
+                    //si el índice no corresponde con la página mostrada actualmente,
+                    echo '  <a href="catalogo-'.$i.'">'.$i.'</a>  ';
+                    //coloco el enlace para ir a esa página
+                 }
+              }
+              if ($pagina != $total_paginas){
+                 echo '<a href="catalogo-'.($pagina+1).'"><i class="fa fa-arrow-right"></i></a>';
+              }
+        }
+  ?>
+
+
+
   <div class="fondoModal"></div>
   <div class="wrap-modalDetalle">
     <div class="modalDetalle">
