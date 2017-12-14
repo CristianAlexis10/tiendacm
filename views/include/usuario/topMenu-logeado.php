a0bc9f21aa010da6bb4a3cd5d7450743df5e5e6a
CristianAlexis10 <cristian1020011@gmail.com>
2017-12-13 21:53:27 -0500

    a

--- views/include/usuario/topMenu-logeado.php
+++ views/include/usuario/topMenu-logeado.php
@@ -4,15 +4,18 @@
      <a href="landing"><img src="views/assets/img/logo_blanco.png"></a>
     </div>
     <ul class="caja--menu">
-      <a href="catalogo" class="catalogo"><li>Catalogo</li></a>
+      <a href="cliente" class="catalogo"><li>Catalogo</li></a>
       <div class="categoria-menu">
         <div class="categorias-items">
-          <h2>camisa</h2>
+        <?php foreach ($this->master->selectAll('categoria') as $row ) {?>
+          <h1><a href="catalogo--<?php echo $row['cat_codigo'] ?>"><?php echo $row['cat_nombre'] ?></a></h1>
+        <?php } ?>
+        <h1><a href="cliente">Todos</a></h1>
         </div>
       </div>
       <a href="videos"><li>Videos</li></a>
       <a href="noticias"><li>Noticias</li></a>
-      <a href="#"><li>julio</li></a>
+      <a href="#"><li><?php echo $_SESSION['USER']['NAME'] ?></li></a>
       <li><i class="fa fa-sign-out" aria-hidden="true"></i></li>
       <li id="btnCarrito"><i class="fa fa-shopping-cart" aria-hidden="true"></i></li>
     </ul>
