50c4c12a63f81a71dcb71e247c485e1db66a9560
CristianAlexis10 <cristian1020011@gmail.com>
2018-05-04 13:16:24 -0500

    sad

--- views/modules/admin/news/index.php
+++ views/modules/admin/news/index.php
@@ -2,29 +2,36 @@
 <section class="contenido" id="cat-conte">
   <h1>Gestion del blog</h1>
   <div class="wrapNewsGrid">
-    <div class="itemNews">
-      <div class="newsTitle">
-        <h2>vamos a ver que sale</h2>
-      </div>
-      <div class="newsImg">
-        <div class="wrapImg">
-          <img src="" alt="">
+
+    <?php
+        $data = $this->master->selectAll("noticia");
+      ?>
+      <?php foreach ($data as $row) { ?>
+        <div class="itemNews">
+          <div class="newsTitle">
+            <h2><?php echo $row['not_titulo']?></h2>
+          </div>
+          <div class="newsImg">
+            <div class="wrapImg">
+              <img src="views/assets/img/news/<?php echo $row['not_poster']?>" alt="">
+            </div>
+          </div>
+          <div class="newsDesc">
+            <div class="text">
+                <?php echo $row['not_preview']?>
+            </div>
+          </div>
+          <div class="newsBtn">
+            <div class="newsEdit" id="entry1">
+              <i class="far fa-edit"></i>
+            </div>
+            <div class="newsDelete">
+              <i class="fas fa-trash-alt"></i>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="newsDesc">
-        <div class="text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </div>
-      <div class="newsBtn">
-        <div class="newsEdit" id="entry1">
-          <i class="far fa-edit"></i>
-        </div>
-        <div class="newsDelete">
-          <i class="fas fa-trash-alt"></i>
-        </div>
-      </div>
-    </div>
+      <?php  }?>
+
     <div class="itemNews">
       <div class="newsTitle" id="newEntry">
         <h2>añadir entrada</h2>
