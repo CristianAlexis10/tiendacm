0f9247ca829b0012789322c19d857b230b18db19
julian <jdgiraldo08@misena.edu.co>
2018-05-04 21:45:32 -0500

    weas

--- views/modules/user/news/index.php
+++ views/modules/user/news/index.php
@@ -10,9 +10,22 @@
 </section>
 <section>
   <div class="wrapNews">
-    <div class="new" id="new1">
+    <div class="new">
       <div class="imgNew">
-        <img src="views/assets/img/defaultProfile.png" alt="">
+        <a href="ver-noticia-"><img src="views/assets/img/defaultProfile.png" alt=""></a>
+      </div>
+      <div class="wrapSummaryNew">
+        <div class="wrapTitle">
+          <a href="ver-noticia-"><h2>title hola</h2></a>
+        </div>
+        <div class="wrapSummary">
+          <a href="ver-noticia-"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></a>
+        </div>
+      </div>
+    </div>
+    <div class="new">
+      <div class="imgNew">
+        <a href="ver-noticia-"><img src="views/assets/img/defaultProfile.png" alt=""></a>
       </div>
       <div class="wrapSummaryNew">
         <div class="wrapTitle">
