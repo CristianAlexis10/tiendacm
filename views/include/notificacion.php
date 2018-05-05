e14d5b39dc6d3829e014115697a6879965def47c
CristianAlexis10 <cristian1020011@gmail.com>
2018-05-04 20:56:40 -0500

    sada

--- views/include/notificacion.php
+++ views/include/notificacion.php
@@ -1,13 +1,14 @@
 <button id="alerta">alerta!</button>
-<div class="wrapAlert">
-  <button class="alert">X</button>
-</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>
 
 <script type="text/javascript">
-
 var msn = "hola";
-
-$("#alerta").click(function() {
+function cerrarAlerta() {
+  $(".wrapAlert").css("transform","translateX(-100%)");
+}
+function alerta(){
+  $("body").append('<div class="wrapAlert" style="width: 350px;height: 150px;  position: absolute;  left: 0px;bottom: 50px;background: #fff;transform: translateX(-100%);transition: .3s;display: grid;grid-template-rows: 25px 125px;box-shadow: 0px 0px 20px -8px black;  padding: 10px;"><button class="alert" style="  width: 25px;  margin-left: 325px;  border: none;  cursor: pointer;  background: #fff;  outline: none;  font-weight: bold;" onclick="cerrarAlerta()">X</button></div>');
   $("p").remove();
   $("<p>"+ msn +"</p>").insertAfter(".alert");
   $(".wrapAlert").css("transform","translateX(0px)");
@@ -15,35 +16,5 @@ $("#alerta").click(function() {
     $(".wrapAlert").css("transform","translateX(-100%)");
     $("p").remove();
   },2000);
-});
-$(".alert").click(function() {
-  $(".wrapAlert").css("transform","translateX(-100%)");
-});
-
+}
 </script>
-
-<style>
-.wrapAlert{
-  width: 350px;
-  height: 150px;
-  position: absolute;
-  left: 0px;
-  bottom: 50px;
-  background: #fff;
-  transform: translateX(-100%);
-  transition: .3s;
-  display: grid;
-  grid-template-rows: 25px 125px;
-  box-shadow: 0px 0px 20px -8px black;
-  padding: 10px;
-}
-.alert{
-  width: 25px;
-  margin-left: 325px;
-  border: none;
-  cursor: pointer;
-  background: #fff;
-  outline: none;
-  font-weight: bold;
-}
-</style>
